3c649c251acb4cdd6defdee8bcb0e6c4a1543a15
aeherman <aeh2196@columbia.edu>
2021-03-05 19:50:04 -0500

    require %>%

--- R/fbind.R
+++ R/fbind.R
@@ -1,16 +1,20 @@
 #' Bind two factors
 #'
 #' Create a new factor from two exisiting factors, where the new factor's levels
-#' are the union of the levels of the input factors.
+#' are the union of the levels of the input factors.  See example of fac and fac2
+#' that are automatically loaded with the package.
 #'
-#' @param a factor
-#' @param b factor
+#' @param fac factor
+#' @param fac2 factor
 #'
 #' @return factor
 #' @export
 #'
 #' @examples
-#' fbind(iris$Species[c(1, 51, 101), PlantGrowth$group[c(1, 11, 21)]])
-fbind <- function(a, b) {
-  factor(c(as.character(a), as.character(b)))
+#' fac <- factor(c("is", "the", "function"))
+#' fac2 <- factor("working")
+#' fbind(fac, fac2)
+fbind <- function(fac, fac2) {
+  require(magrittr, "%>%")
+  c(as.character(fac), as.character(fac2)) %>% factor()
 }
